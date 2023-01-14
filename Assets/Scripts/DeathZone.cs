03a9d893a354011dc46c8da17730070f384b1dd4
Brian Ho <brianslho@gmail.com>
2023-01-13 21:57:03 -0800

    feat: game over condition

--- Assets/Scripts/DeathZone.cs
+++ Assets/Scripts/DeathZone.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class DeathZone : MonoBehaviour
 {
+    public float DEATH_DELAY = 2.0f;
+    
+    public GameManager gameManager;
+    public HashSet<GameObject> capoosInside = new HashSet<GameObject>(); // Array of Capoos currently inside the DeathZone
+    public float capoosInsideElapsedTime = 0.0f; // Continuous amount of time that there have been Capoos inside the DeathZone
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,52 @@ public class DeathZone : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        // Note: need to consider whether the capoo currently inside is the currentCapoo!
+        if (capoosInside.Count == 0 || (capoosInside.Count == 1 && gameManager.currentCapoo && capoosInside.Contains(gameManager.currentCapoo)))
+        {
+            capoosInsideElapsedTime = 0.0f;
+        }
+        else {
+            capoosInsideElapsedTime += Time.deltaTime;
+            if (capoosInsideElapsedTime >= DEATH_DELAY) {
+                Debug.Log("GAME OVER");
+                // TODO: Game Over State
+            }
+        }
+    }
+
+    // If any Capoos collide with the DeathZone, log a message
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Capoo1") ||
+            other.gameObject.CompareTag("Capoo2") ||
+            other.gameObject.CompareTag("Capoo3") ||
+            other.gameObject.CompareTag("Capoo4") ||
+            other.gameObject.CompareTag("Capoo5") ||
+            other.gameObject.CompareTag("Capoo6") ||
+            other.gameObject.CompareTag("Capoo7") ||
+            other.gameObject.CompareTag("Capoo8"))
+        {
+            Debug.Log("Capoo entered DeathZone");
+            capoosInside.Add(other.gameObject);
+        }
+    }
+
+    // If any Capoos leave the DeathZone, log a message
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Capoo1") ||
+            other.gameObject.CompareTag("Capoo2") ||
+            other.gameObject.CompareTag("Capoo3") ||
+            other.gameObject.CompareTag("Capoo4") ||
+            other.gameObject.CompareTag("Capoo5") ||
+            other.gameObject.CompareTag("Capoo6") ||
+            other.gameObject.CompareTag("Capoo7") ||
+            other.gameObject.CompareTag("Capoo8"))
+        {
+            Debug.Log("Capoo left DeathZone");
+            capoosInside.Remove(other.gameObject);
+        }
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameLogic : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
-    private float RESPAWN_DELAY = 0.8f;
-    private float DEATH_DELAY = 2.0f;
+    public float RESPAWN_DELAY = 0.8f;
+
+    public DeathZone deathZone;
 
     GameObject Capoo1;
     GameObject Capoo2;
@@ -16,12 +17,8 @@ public class GameLogic : MonoBehaviour
     GameObject Capoo7;
     GameObject Capoo8;
 
-    private GameObject currentCapoo;
+    public GameObject currentCapoo;
     private float respawnCountdown = 0.0f;
-
-    public GameObject DeathLine; // If Capoos stay above this line for too long, you lose
-    private bool capoosAboveLineBefore; // Keeps track of whether Capoos were above the line on the previous frame
-    private float capoosAboveLineElapsedTime = 0.0f; // Keeps track of the continuous time that Capoos have been above the line
     
     // Start is called before the first frame update
     void Start()
@@ -44,57 +41,19 @@ public class GameLogic : MonoBehaviour
     // Update is called once per frame
     void Update()
     {        
-        // Get all Capoos in the scene (from Capoo1 through Capoo8)
-        GameObject[] allCapoo1s = GameObject.FindGameObjectsWithTag("Capoo1");
-        GameObject[] allCapoo2s = GameObject.FindGameObjectsWithTag("Capoo2");
-        GameObject[] allCapoo3s = GameObject.FindGameObjectsWithTag("Capoo3");
-        GameObject[] allCapoo4s = GameObject.FindGameObjectsWithTag("Capoo4");
-        GameObject[] allCapoo5s = GameObject.FindGameObjectsWithTag("Capoo5");
-        GameObject[] allCapoo6s = GameObject.FindGameObjectsWithTag("Capoo6");
-        GameObject[] allCapoo7s = GameObject.FindGameObjectsWithTag("Capoo7");
-        GameObject[] allCapoo8s = GameObject.FindGameObjectsWithTag("Capoo8");
-        
-        // Check if any Capoos are above the death line
-        bool capoosAboveLineNow = false;
-        foreach (GameObject[] capoos in new GameObject[][] { allCapoo1s, allCapoo2s, allCapoo3s, allCapoo4s, allCapoo5s, allCapoo6s, allCapoo7s, allCapoo8s }) {
-            foreach (GameObject capoo in capoos) {
-                if (capoo == currentCapoo) {
-                    // Ignore the currentCapoo
-                    continue;
-                }
-                if (capoo.transform.position.y > DeathLine.transform.position.y) {
-                    // If there were already Capoos above the death line, increment the timer
-                    if (capoosAboveLineBefore == true) {
-                        capoosAboveLineElapsedTime += Time.deltaTime;
-                        if (capoosAboveLineElapsedTime > DEATH_DELAY) {
-                            // TODO: Game over state
-                            Debug.Log("Game over!");
-                        }
-                    }
-                    capoosAboveLineNow = true;
-                    capoosAboveLineBefore = true;
-                    break;
-                }
-            }
-        }
-        // If none of the Capoos are above the death line, reset the timer
-        if (!capoosAboveLineNow) {
-            capoosAboveLineElapsedTime = 0.0f;
-        }
-
-        // Get mouse position in world space
-        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
         // Count down the respawn timer
         if (respawnCountdown > 0 && currentCapoo == null) {
             respawnCountdown -= Time.deltaTime;
             // During this time, there is nothing else to do, so return
             return;
         }
+
+        // Get mouse position in world space
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         
         // If there is no currentCapoo, spawn a new random Capoo and set it as currentCapoo.
-        // NOTE: If there are still Capoos above the death line, don't spawn a new Capoo! This could cause a Capoo explosion.
-        else if (currentCapoo == null && capoosAboveLineNow == false) {
+        // If there are still Capoos in the death zone, don't spawn a new Capoo! This could cause a Capoo explosion.
+        if (currentCapoo == null && deathZone.capoosInside.Count == 0) {
             // Spawn a random Capoo between Capoo1 and Capoo3
             if (Random.Range(0, 2) == 0) {
                 currentCapoo = Instantiate(Capoo1, new Vector3(mouseWorldPos.x, 4, 0), Quaternion.identity);
