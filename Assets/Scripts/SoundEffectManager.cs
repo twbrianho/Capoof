22943e51330e8d7c5c28218bf6f4d72882c6b585
Brian Ho <brianslho@gmail.com>
2023-01-14 22:35:52 -0800

    feat: more wahs

--- Assets/Scripts/SoundEffectManager.cs
+++ Assets/Scripts/SoundEffectManager.cs
@@ -14,6 +14,12 @@ public class SoundEffectManager : MonoBehaviour
     public AudioClip wah1Sound;
     public AudioClip wah2Sound;
     public AudioClip wah3Sound;
+    public AudioClip wah4Sound;
+    public AudioClip wah5Sound;
+    public AudioClip wah6Sound;
+    public AudioClip wah7Sound;
+    public AudioClip wah8Sound;
+    public AudioClip wah9Sound;
 
     // Start is called before the first frame update
     void Start()
@@ -29,16 +35,34 @@ public class SoundEffectManager : MonoBehaviour
 
     public void PlayCapooLaunchSound()
     {
-        int randomSound = Random.Range(1, 4);
+        int randomSound = Random.Range(1, 10);
         switch (randomSound) {
             case 1:
-                audioSource.PlayOneShot(wah1Sound, 0.5f);
+                audioSource.PlayOneShot(wah1Sound);
                 break;
             case 2:
-                audioSource.PlayOneShot(wah2Sound, 0.5f);
+                audioSource.PlayOneShot(wah2Sound);
                 break;
             case 3:
-                audioSource.PlayOneShot(wah3Sound, 0.5f);
+                audioSource.PlayOneShot(wah3Sound);
+                break;
+            case 4:
+                audioSource.PlayOneShot(wah4Sound);
+                break;
+            case 5:
+                audioSource.PlayOneShot(wah5Sound);
+                break;
+            case 6:
+                audioSource.PlayOneShot(wah6Sound);
+                break;
+            case 7:
+                audioSource.PlayOneShot(wah7Sound);
+                break;
+            case 8:
+                audioSource.PlayOneShot(wah8Sound);
+                break;
+            case 9:
+                audioSource.PlayOneShot(wah9Sound);
                 break;
         }
     }
